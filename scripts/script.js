4348df38f6dca52517869a9d7394a274f7763c85
Egor <revival1002@gmail.com>
2022-09-16 18:25:33 +0000

    Исправление ошибок

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,25 @@
 let editButton = document.querySelector('.profile__info-edit');
 let closeButton = document.querySelector('.popup__close');
-
 let nameInput = document.querySelector('.popup__form-input[name="name"]');
 let descriptionInput = document.querySelector('.popup__form-input[name="description"]');
 let nameText = document.querySelector('.profile__info-name');
 let descText = document.querySelector('.profile__info-description');
-
 let saveForm = document.querySelector('#popup__profile');
-
-nameInput.value = nameText.textContent;
-descriptionInput.value = descText.textContent;
+let popup = document.querySelector('.popup');
 
 function togglePopup() {
-	let popup = document.querySelector('.popup');
 	popup.classList.toggle('popup_opened');
+	nameInput.value = nameText.textContent;
+	descriptionInput.value = descText.textContent;
 }
 
-function saveData() {
+function saveData(evt) {
+	evt.preventDefault();
 	nameText.textContent = nameInput.value;
 	descText.textContent = descriptionInput.value;
+	togglePopup();
 }
 
 editButton.addEventListener('click', togglePopup);
 closeButton.addEventListener('click', togglePopup);
-saveForm.addEventListener('submit', function(evt) {
-	evt.preventDefault();
-	saveData();
-	togglePopup();
-});
+saveForm.addEventListener('submit', saveData);
